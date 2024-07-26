2d4d2571746dc8313c47f30c5b34f1bbeb404af4
Rey Obejero <rey_obejero@dlsu.edu.ph>
2024-07-26 18:07:09 +0800

    feat: Add input validation for delete cart item

--- server/src/routes/cart.router.ts
+++ server/src/routes/cart.router.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { cartController } from '@/controllers';
 import { authenticate, protect, validate } from '@/middlewares';
-import { handlecartItemSchema } from '@/validations';
+import { deleteCartItemSchema, handleCartItemSchema } from '@/validations';
 
 const cartRouter = Router();
 
-cartRouter.post('/:productId/:packageId', authenticate, validate(handlecartItemSchema), cartController.handleCartItem);
+cartRouter.post('/:productId/:packageId', authenticate, validate(handleCartItemSchema), cartController.handleCartItem);
 cartRouter.get('/', cartController.getCarts);
 cartRouter.get('/me', authenticate, cartController.getAuthenticatedUserCart);
-cartRouter.delete('/', authenticate, cartController.deleteCarts);
+cartRouter.delete('/', authenticate, validate(deleteCartItemSchema), cartController.deleteCarts);
 cartRouter.delete('/:productId/:packageId', authenticate, protect, cartController.deleteCartItem);
 
 export { cartRouter };
--- server/src/validations/cart.validation.ts
+++ server/src/validations/cart.validation.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { errorMessages } from '@/constants';
 
-export const handlecartItemSchema = z.object({
+export const handleCartItemSchema = z.object({
     params: z.object({
         productId: z
             .string({ message: errorMessages.PRODUCT_ID_INVALID })
@@ -14,5 +14,21 @@ export const handlecartItemSchema = z.object({
     }),
     body: z.object({
         quantity: z.number({ message: errorMessages.QUANTITY_INVALID }),
+    }),
+});
+
+export const deleteCartItemSchema = z.object({
+    params: z.object({
+        productId: z
+            .string({ message: errorMessages.PRODUCT_ID_INVALID })
+            .transform((productId) => Number(productId))
+            .refine((productId) => !isNaN(productId), { message: errorMessages.PRODUCT_ID_INVALID }),
+        packageId: z
+            .string({ message: errorMessages.PACKAGE_ID_INVALID })
+            .transform((packageId) => Number(packageId))
+            .refine((packageId) => !isNaN(packageId), { message: errorMessages.PACKAGE_ID_INVALID }),
+    }),
+    body: z.object({
+        cartId: z.number({ message: errorMessages.CART_ID_INVALID }),
     }),
 });
