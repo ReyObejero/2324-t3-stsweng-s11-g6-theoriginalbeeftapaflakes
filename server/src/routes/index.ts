3bf9b4eb59b84954ba792c12542009cb27b43e55
Rey Obejero <rey_obejero@dlsu.edu.ph>
2024-07-28 22:22:43 +0800

    feat: Implement CRUD operations for static pages

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,4 +2,5 @@ export * from './auth.router';
 export * from './cart.router';
 export * from './product.router';
 export * from './review.router';
+export * from './static-page.router';
 export * from './user.router';
