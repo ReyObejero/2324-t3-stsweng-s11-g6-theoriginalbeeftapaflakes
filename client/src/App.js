3b9aeed06af5a7d748a6f17faa66fab5bd12d01d
Rey Obejero <rey_obejero@dlsu.edu.ph>
2024-08-01 18:59:50 +0800

    feat: Add authentication

---
+++ client/src/App.js
@@ -0,0 +1,93 @@
+import './App.css';
+import Collapsible from 'react-collapsible';
+import Navbar from './Components/Navbar/Navbar.jsx';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Views/Home/Home.jsx';
+import Products from './Components/Views/ProductList_Views/ProductList_Views.jsx';
+import ProductList from './Components/ProductList/ProductList.jsx';
+import About from './Components/Views/About/About.jsx';
+import ProductPage from './Components/Views/Products/ProductPage_Views.jsx';
+import Login from './Components/Views/Login/Login_Views.jsx';
+import Register from './Components/Views/Register/Register_Views.jsx';
+import CreateAdmin from './Components/Views/CreateAdmin/CreateAdmin_Views.jsx';
+import ForgotPassword from './Components/Views/ForgotPassword_Views/ForgotPassword_Views.jsx';
+import Cart from './Components/Views/Cart/Cart.jsx';
+import AdminDashboard from './Components/Views/Admin/Admin.jsx';
+import OrderManagement from './Components/Views/OrderManagement/OrderManagement_Views.jsx';
+import { decodeToken } from 'react-jwt';
+import CheckoutandStatus from './Components/Views/CheckoutandStatus/CS.jsx';
+
+function App() {
+    const [token, setToken] = useState(localStorage.getItem('jwt'));
+    const decodedToken = token ? decodeToken(token) : null;
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // Initialize isLoggedIn state
+
+    const isTokenExpired = (token) => {
+        if (!token) {
+            setIsLoggedIn(false); // Set isLoggedIn to false if there's no token
+            return false;
+        }
+        setIsLoggedIn(true); // Set isLoggedIn to true if there's a token
+        const expirationTime = decodedToken.exp * 1000;
+        return Date.now() >= expirationTime;
+    };
+
+    // Function to handle logout
+    const logout = () => {
+        localStorage.removeItem('jwt'); // Clear token from storage
+        setIsLoggedIn(false); // Set isLoggedIn to false when logging out
+        alert('Your session has expired. You have been logged out.');
+        window.location.href = '/login';
+    };
+
+    // Check token expiration on component mount and token change
+    useEffect(() => {
+        const handleTokenChange = () => {
+            setToken(localStorage.getItem('jwt'));
+        };
+
+        window.addEventListener('storage', handleTokenChange);
+
+        if (token && isLoggedIn) {
+            logout(); // Token is expired, log out user
+        } else if (token && decodedToken) {
+            const expirationTime = decodedToken.exp * 1000;
+            const timeToExpire = expirationTime - Date.now();
+            if (timeToExpire > 0) {
+                setTimeout(() => {
+                    logout(); // Logout user after token expiration time
+                }, timeToExpire);
+            }
+        }
+
+        return () => {
+            window.removeEventListener('storage', handleTokenChange);
+        };
+    }, [token]);
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/products/:productId" element={<ProductPage />} />
+                    <Route path="/products" element={<Products category="list" />} />
+                    <Route path="/about/*" element={<About />} />
+                    <Route path="/product-management" element={<AdminDashboard />} />
+                    <Route path="/order-management" element={<OrderManagement />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/productlist" element={<ProductList />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/createadmin" element={<CreateAdmin />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route path='/COS' element={<CheckoutandStatus />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/Components/Products/ProductPage.jsx
@@ -0,0 +1,207 @@
+import React, { useState, useEffect } from 'react';
+import './ProductPage.css';
+import { Link } from 'react-router-dom';
+import { PRODUCT_URL, CARTS_URL } from '../../API/constants';
+import axiosInstance from '../../API/axiosInstance.js';
+import Cart from '../Views/Cart/Cart.jsx';
+
+const Product = () => {
+    // Scroll to top on mount
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    const [product, setProduct] = useState();
+    const [selectedPackage, setSelectedPackage] = useState('');
+    const [quantity, setQuantity] = useState(1);
+    const [warningMessage, setWarningMessage] = useState('');
+    const [showWarning, setShowWarning] = useState(false);
+    const [token, setToken] = useState(localStorage.getItem('jwt'));
+    const [addedToCartMessage, setAddedToCartMessage] = useState('');
+
+    useEffect(() => {
+        const handleFetchProduct = async () => {
+            try {
+                const productId = window.location.pathname.split('/').pop();
+                const response = await axiosInstance.get(`${PRODUCT_URL}/${productId}`, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+                    },
+                });
+
+                if (response.status !== 200) {
+                    throw new Error(response.data.error || 'Failed to fetch product');
+                }
+
+                setProduct(response.data);
+            } catch (error) {
+                console.error('Error fetching product:', error.message);
+            }
+        };
+
+        handleFetchProduct();
+    }, []);
+
+    // Function to update the selected package
+    const handlePackageSelection = (productpackage) => {
+        setSelectedPackage((prevPackage) => (prevPackage === productpackage ? '' : productpackage));
+        setShowWarning(false);
+    };
+
+    // Function to get the price based on package
+    const getPrice = (price) => {
+        const numericPrice = parseFloat(price.$numberDecimal);
+        return numericPrice.toLocaleString('en-PH', { style: 'currency', currency: 'PHP' });
+    };
+
+    // Function to get the total bottles based on selected package
+    const getTotalBottles = (pkgIndex) => {
+        if (pkgIndex === -1) return null;
+        let total = 0;
+        product.packages[pkgIndex].bottlesPerFlavor.forEach((bottle) => {
+            total += bottle.quantity;
+        });
+        return `${total} Bottles/Box `;
+    };
+
+    const getProductSize = (pkgIndex) => {
+        if (pkgIndex === -1) return null;
+        const size = product.packages[pkgIndex].packageSize;
+        return `(${size} Grams)`;
+    };
+
+    const handleAddToCart = async () => {
+        const productId = window.location.pathname.split('/').pop();
+        const packageData = product.packages.find((pkg) => pkg.packageOption === selectedPackage);
+        if (!selectedPackage) {
+            setWarningMessage('Please select a package');
+            setShowWarning(true);
+        } else {
+            setShowWarning(false); // Reset warning message
+            try {
+                // Prepare the cart item data
+                const cartItem = {
+                    productId: productId,
+                    name: product.name,
+                    selectedPackage: selectedPackage,
+                    size: packageData.packageSize,
+                    price: packageData.price,
+                    quantity: quantity,
+                };
+                // Make a POST request to add the item to the cart
+                const response = await axiosInstance.post(`${CARTS_URL}/add`, cartItem, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                // Check if the request was successful
+                if (response.status === 201) {
+                    setAddedToCartMessage(response.data.message);
+                    // Reset the message after 3 seconds
+                    setTimeout(() => {
+                        setAddedToCartMessage('');
+                    }, 3000);
+                } else {
+                    throw new Error(response.data.error || 'Failed to add product to cart');
+                }
+            } catch (error) {
+                console.error('Error adding product to cart:', error.message);
+                // Optionally, you can display an error message to the user
+            }
+        }
+    };
+
+    const handleQuantityChange = (e) => {
+        const newQuantity = parseInt(e.target.value, 10);
+        if (newQuantity >= 1) {
+            setQuantity(newQuantity);
+        }
+    };
+
+    return (
+        <div className="product-container">
+            {product && (
+                <div className="p-details-container">
+                    <div className="p-image-gallery">
+                        <img src={`https://tobtf.onrender.com/${product.image}`} alt={product.name} />
+                    </div>
+                    <div className="product-details">
+                        <h1>{product.name}</h1>
+                        <p className="p-amount">
+                            {getTotalBottles(
+                                product.packages.findIndex((pkg) => pkg.packageOption === selectedPackage),
+                            )}
+                            {getProductSize(product.packages.findIndex((pkg) => pkg.packageOption === selectedPackage))}
+                        </p>
+                        <p className="p-price">
+                            {selectedPackage
+                                ? getPrice(product.packages.find((pkg) => pkg.packageOption === selectedPackage).price)
+                                : 'Select a package'}
+                        </p>
+                        <div>
+                            {product.packages.map((productpackage, index) => (
+                                <button
+                                    key={index}
+                                    onClick={() => handlePackageSelection(productpackage.packageOption)}
+                                    className={
+                                        selectedPackage === productpackage.packageOption
+                                            ? 'p-package-button selected'
+                                            : 'p-package-button'
+                                    }
+                                >
+                                    {productpackage.packageOption}
+                                </button>
+                            ))}
+                        </div>
+                        <ul>
+                            {product.packages
+                                .filter((pckg) => pckg.packageOption === selectedPackage)
+                                .flatMap((pckg) =>
+                                    pckg.bottlesPerFlavor.map((flavor, index) => (
+                                        <li key={index}>
+                                            {flavor.flavor}: {flavor.quantity}
+                                        </li>
+                                    )),
+                                )}
+                        </ul>
+                        {showWarning && <div className="p-error-bubble">{warningMessage}</div>}
+                        <h3>Product Description:</h3>
+                        <p className="p-product-description" style={{ whiteSpace: 'pre-line' }}>
+                            {product.description}
+                        </p>
+                        <div className="p-quantity-selector">
+                            <label htmlFor="quantity">Quantity:</label>
+                            <input
+                                type="number"
+                                id="quantity"
+                                name="quantity"
+                                min="1"
+                                value={quantity}
+                                onChange={handleQuantityChange}
+                            />
+                        </div>
+                        <ul>
+                            <li>Ingredients: {product.ingredients}</li>
+                            <li>Nutritional Info: {product.nutriInfo}</li>
+                        </ul>
+                        {token ? (
+                            <button className="p-add-to-cart" onClick={handleAddToCart}>
+                                ADD TO CART
+                            </button>
+                        ) : (
+                            <Link to={'/login'}>
+                                <button className="p-add-to-cart">ADD TO CART</button>
+                            </Link>
+                        )}
+                        {addedToCartMessage && <div className="p-success-message">{addedToCartMessage}</div>}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Product;
