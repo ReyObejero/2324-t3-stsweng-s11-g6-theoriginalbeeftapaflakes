69f78af277d5907dc3d59df37196cb1b7b64303c
Rey Obejero <rey_obejero@dlsu.edu.ph>
2024-08-03 04:59:08 +0800

    chore: Testing

--- server/build/app.js
+++ server/build/app.js
@@ -13,7 +13,7 @@ const app = (0, express_1.default)();
 exports.app = app;
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
 }));
 app.use(express_1.default.json());
--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: process.env.CORS_ORIGIN,
         credentials: true,
     }),
 );
