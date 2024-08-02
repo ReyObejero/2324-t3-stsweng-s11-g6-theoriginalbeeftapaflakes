a90e0fd41573a0181c082c478ce48b5a9aa02bc5
Rey Obejero <rey_obejero@dlsu.edu.ph>
2024-08-03 03:44:20 +0800

    fix: Fix CORS configuration

--- server/build/app.js
+++ server/build/app.js
@@ -7,12 +7,15 @@ exports.app = void 0;
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
+const config_1 = require("./config");
 const middlewares_1 = require("./middlewares");
 const routes_1 = require("./routes");
 const app = (0, express_1.default)();
 exports.app = app;
 app.use((0, cookie_parser_1.default)());
+console.log(config_1.env.cors.CORS_ORIGIN);
 app.use((0, cors_1.default)({
+    origin: config_1.env.cors.CORS_ORIGIN,
     credentials: true,
 }));
 app.use(express_1.default.json());
