65d418930f792ec1eeb0bcb5b68923eabcbb4a06
Rey Obejero <rey_obejero@dlsu.edu.ph>
2024-08-03 03:33:19 +0800

    fix: Fix CORS configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,6 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
-import { env } from './config';
 import { errorHandler } from './middlewares';
 import { mountRoutes } from './routes';
 
@@ -9,7 +8,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
     cors({
-        origin: env.cors.CORS_ORIGIN,
+        origin: process.env.CORS_ORIGIN,
         credentials: true,
     }),
 );
