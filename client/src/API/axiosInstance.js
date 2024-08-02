aacc00c5074dbcf2f309482b80789c9209a9e096
Rey Obejero <rey_obejero@dlsu.edu.ph>
2024-08-03 03:18:38 +0800

    fix: Fix prisma client generation for deployment

--- client/src/API/axiosInstance.js
+++ client/src/API/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: `https://2324-t3-stsweng-s11-g6-theoriginalbeeftapaflakes-bpekbxjy5.vercel.app/`, // Backend Server URL
+    baseURL: `https://2324-t3-stsweng-s11-g6-theoriginalbeeftapaflakes-bpekbxjy5.vercel.app/`,
+    withCredentials: true,
 });
 
 export default instance;
